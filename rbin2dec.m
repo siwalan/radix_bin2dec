4a2a69a0be906d554efc9fce23cb1c07656c12e7
siwalan <59278902+siwalan@users.noreply.github.com>
2021-03-28 19:06:48 +0800

    Update rbin2dec.m

--- rbin2dec.m
+++ rbin2dec.m
@@ -1,12 +1,18 @@
 function dec = radix_bin2dec(bin_vector, radix)
-    if (ischar(bin_vector) == 0)
+    if (ischar(bin_vector))
         for i=1:strlength(bin_vector)
-            bin_vector(i) = str2num(bin_vector(i));
+            bin_vector(i) = str2num(bin_vector(i)); %#ok<ST2NM>
         end
     end
     dec = 0;
     vector_length = length(bin_vector);
-    for i=1:vector_length
-        dec = dec + bin_vector(i) * 2^(vector_length-radix+1-i)
+    if radix ~=0
+        for i=1:vector_length
+            dec = dec + bin_vector(i) * 2^(vector_length-radix+1-i);
+        end
+    else
+       for i=1:vector_length
+         dec = dec + bin_vector(i) * 2^(vector_length-i);
+        end
     end
 end
